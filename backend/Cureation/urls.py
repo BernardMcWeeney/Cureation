bada5f76301453c50ec4ff5c7601a6a44ec2db79
Bernard McWeeney <bernard@greenberry.ie>
2023-01-05 20:42:39 +0000

    Adding settings

--- backend/Cureation/urls.py
+++ backend/Cureation/urls.py
@@ -33,13 +33,13 @@ router.register(r'api_users', views.APIUserViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(r'api/v2/', include(router.urls)),
+    path('api/v2/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/wag/v2/', api_router.urls),
     path('cms/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
     path('pages/', include(wagtail_urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
